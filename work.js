2ba7bbc989e1a5ffa3281b8ca9d485b05a17f727
twill4317 <williamst0001@gmail.com>
2021-06-28 13:12:14 -0400

    added functionality to board

--- work.js
+++ work.js
@@ -4,11 +4,13 @@ const BASEELEMENT = document.querySelectorAll('#card')
 
 BASEELEMENT.forEach(function(card){
     card.addEventListener('click', function(event){
-        if (secretNumber == event.innerHTML){
-            GAMETRON.innerHTML = "You Win"
-        }
+        game.changeColor(card, card.innerHTML )
+            if(card.innerHTML == game.secretNumber){
+                GAMETRON.innerHTML = 'You Win ! ! !'
+            }
+        })
     })
-})
+
 
 const game = { 
     secretNumber:0,
@@ -21,14 +23,38 @@ const game = {
         this.secretNumber = number
     },
     
-    changeTron: function (){
-        GAMETRON.innerHTML = this.secretNumber
+    resetTron: function (){
+        GAMETRON.innerHTML = 'Guess my number ("/)'
+    },
+
+    changeColor: function(card,numtwo){
+        difference = Math.abs(game.secretNumber - numtwo)
+        
+            if(difference == 0){
+                card.setAttribute("style","background-color: gold;")
+            } else if (difference >= 1 && difference < 5) {
+                card.setAttribute("style","background-color: red;")
+            } else if (difference >= 5 && difference < 10){
+                card.setAttribute("style", "background-color: orange;")
+            }
+            else if (difference >= 10 && difference < 15){
+                card.setAttribute("style","background-color: aquamarine")
+            } else if (difference >= 15 && difference < 20){
+                card.setAttribute("style","background-color:aqua;")
+            } else if (difference >= 20 && difference < 30){
+                card.setAttribute("style","background-color:celeste;")
+            } else if  (difference >= 30 && difference < 50){  
+                card.setAttribute("style","background-color:light-cyan;")
+            } else if(difference > 50){
+                card.setAttribute("style", "background-color: white;")
+            }
     },
 
     start: function(){
         this.setSecretNumber(this.getRandom(100,1))
-        this.changeTron()
+        this.resetTron()
     }
+
 }
 
 game.start()
